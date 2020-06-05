96521dc0b4df8bb3b08cc21b76308ec6a0523778
Phil Hunt <phil@deviantlogic.co.uk>
2020-06-05 12:52:35 +0100

    some routes and wrap-relod

--- project.clj
+++ project.clj
@@ -1,7 +1,10 @@
 (defproject todo-list "0.1.0-SNAPSHOT"
-  :description "Todo list application, based on Practicalli, learning Ring"
-  :url "http://example.com/FIXME"
+  :description "Todo webapp, based on Practicalli, learning Ring/Compojure"
+  :url "https://github.com/PMHunt/todo-list"
   :license {:name "EPL-2.0 OR GPL-2.0-or-later WITH Classpath-exception-2.0"
             :url "https://www.eclipse.org/legal/epl-2.0/"}
-  :dependencies [[org.clojure/clojure "1.10.1"]]
-  :repl-options {:init-ns todo-list.core})
+  :dependencies [[org.clojure/clojure "1.10.1"]
+                 [ring "1.8.1"]]
+  :repl-options {:init-ns todo-list.core}
+  :main todo-list.core
+  :profiles {:dev {:main todo-list.core/-dev-main}})
--- src/todo_list/core.clj
+++ src/todo_list/core.clj
@@ -1,6 +1,30 @@
-(ns todo-list.core)
+(ns todo-list.core
+  (:require [ring.adapter.jetty :as http]
+            [clojure.pprint :as pp]
+            [ring.middleware.reload :refer [wrap-reload]]))
 
-(defn foo
-  "I don't do a whole lot."
-  [x]
-  (println x "Hello, World!"))
+(defn handler
+  [request]
+  (if (= "/" (:uri request))
+    {:status 200
+     :headers {}
+     :body  "<h1> Hello </h1>
+      <p> I'm another fucking todo app with a handler function </p>"}
+    {:status 404
+     :body "<h1> Nope </h1> <p> No such page, tough titty"
+     :headers {}}))
+
+(defn -dev-main
+  "simple http server usign Ring & Jetty"
+  [port-number]
+  (http/run-jetty
+   (wrap-reload #'handler)
+   {:port (Integer. port-number)
+    :join? false}))
+
+(defn -main
+  "simple http server usign Ring & Jetty"
+  [port-number]
+  (http/run-jetty
+   handler
+   {:port (Integer. port-number)}))
